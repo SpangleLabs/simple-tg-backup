30328e6bfe485dca66e3b971ec6ae8c1df2518a0
JC <joshua@coales.co.uk>
2023-09-07 23:35:19 +0100

    Gather DLResources when scraping a chat

--- tg_backup/target.py
+++ tg_backup/target.py
@@ -2,12 +2,12 @@ import base64
 import datetime
 import json
 import logging
-from typing import Dict
+from typing import Dict, Set, Type
 
 from telethon import TelegramClient
 from tqdm import tqdm
 
-from tg_backup.encoding import encode_message
+from tg_backup.encoding import encode_message, DLResource
 from tg_backup.tg_utils import get_message_count, get_chat_name
 
 
@@ -33,6 +33,7 @@ async def backup_target(client: TelegramClient, target: Dict) -> None:
     latest_id = None
     logger.info("Backing up target chat: %s", chat_name)
     print(f"- Updating {chat_name} logs")
+    total_resources: Dict[Type, Set[DLResource]] = {}
     with tqdm(total=count) as bar:
         async for message in client.iter_messages(entity):
             if latest_id is None:
@@ -43,7 +44,13 @@ async def backup_target(client: TelegramClient, target: Dict) -> None:
             else:
                 encoded_msg = encode_message(message)
                 msg_dict = encoded_msg.raw_data
+                for resource in encoded_msg.downloadable_resources:
+                    if type(resource) not in total_resources:
+                        total_resources[type(resource)] = set()
+                    total_resources[type(resource)].add(resource)
                 print(json.dumps(msg_dict, default=encode_json_extra))
+                total_resource_count = sum([len(x) for x in total_resources.values()])
+                print(f"Gathered {total_resource_count} unique resources: " + ", ".join(f"{key.__name__}: {len(val)}" for key, val in total_resources.items()))
             print(f"Done {bar.n} messages")
             bar.update(1)
 
