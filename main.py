140720e025fd3ec90154f0467f59f3b7b8cdc277
JC <joshua@coales.co.uk>
2023-09-12 09:48:05 +0100

    Setup logging properly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import asyncio
 import logging
+import os
+import sys
+from logging.handlers import TimedRotatingFileHandler
 
 from telethon import TelegramClient
 
@@ -7,10 +10,26 @@ from tg_backup.config import load_config
 from tg_backup.target import BackupTask
 
 logger = logging.getLogger(__name__)
-logging.getLogger().setLevel(logging.DEBUG)
+
+
+def setup_logging() -> None:
+    os.makedirs("logs", exist_ok=True)
+    formatter = logging.Formatter("{asctime}:{levelname}:{name}:{message}", style="{")
+
+    base_logger = logging.getLogger()
+    base_logger.setLevel(logging.INFO)
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(formatter)
+    base_logger.addHandler(console_handler)
+
+    backup_logger = logging.getLogger("tg_backup")
+    file_handler = TimedRotatingFileHandler("logs/tg_backup.log", when="midnight")
+    file_handler.setFormatter(formatter)
+    backup_logger.addHandler(file_handler)
 
 
 def main() -> None:
+    setup_logging()
     conf = load_config()
     client = TelegramClient('simple_backup', conf.client.api_id, conf.client.api_hash)
     client.start()
