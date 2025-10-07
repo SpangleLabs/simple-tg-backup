5482f682d0a6a06b7cc897a4bb7bf31925205ff9
Roselyn Coales <roselyn@coales.co.uk>
2025-10-07 07:25:41 +0100

    Allow encoding Dialogs as json

--- tg_backup/utils/json_encoder.py
+++ tg_backup/utils/json_encoder.py
@@ -7,5 +7,7 @@ def encode_json_extra(value: object) -> str:
         return base64.b64encode(value).decode('ascii')
     elif isinstance(value, datetime.datetime):
         return value.isoformat()
+    elif hasattr(value, "to_dict"):
+        return value.to_dict()
     else:
         raise ValueError(f"Unrecognised type to encode: {value}")
