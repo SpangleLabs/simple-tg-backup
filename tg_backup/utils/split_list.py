f9895ac7b570ba40ee88ffc1210798e3af105c99
Roselyn Coales <roselyn@coales.co.uk>
2025-11-13 19:26:51 +0000

    Rename split_list() to partition_list() and write new split_list() which is a special-case for booleans

--- tg_backup/utils/split_list.py
+++ tg_backup/utils/split_list.py
@@ -5,7 +5,8 @@ from typing_extensions import TypeVar
 Item = TypeVar("Item")
 ItemKey = TypeVar("ItemKey")
 
-def split_list(items: Iterable[Item], condition: Callable[[Item], ItemKey]) -> dict[ItemKey, list[Item]]:
+
+def partition_list(items: Iterable[Item], condition: Callable[[Item], ItemKey]) -> dict[ItemKey, list[Item]]:
     results = {}
     for item in items:
         item_key = condition(item)
@@ -13,3 +14,8 @@ def split_list(items: Iterable[Item], condition: Callable[[Item], ItemKey]) -> d
             results[item_key] = []
         results[item_key].append(item)
     return results
+
+
+def split_list(items: Iterable[Item], condition: Callable[[Item], bool]) -> tuple[list[Item], list[Item]]:
+    results = partition_list(items, condition)
+    return results.get(True), results.get(False)
